c0d151958d6ddde90604a873b9583361e6456efd
Gabriel Pacheco Passos da Silva <Pacheco_gbr@hotmail.com>
2023-05-05 17:33:44 -0300

    Adding an environment variable because of the TypeORM

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,9 @@
-import app from "./config/app";
 import "dotenv/config";
+import { appDataSource } from "../app/shared/infra/db/data-source";
+import app from "./config/app";
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log("The server is running."));
+appDataSource.initialize().then(() => {
+    app.listen(port, () => console.log("The server is running."));
+});
