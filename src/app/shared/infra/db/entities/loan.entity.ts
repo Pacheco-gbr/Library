99ca5cc0a7a7e75c7d2eb421e1464ff2ab666750
Gabriel Pacheco Passos da Silva <Pacheco_gbr@hotmail.com>
2023-05-06 16:36:41 -0300

    Creating the LoanEntity

---
+++ src/app/shared/infra/db/entities/loan.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, OneToOne } from "typeorm";
+import BaseEntity from "./base-entity.entity";
+import { StudentEntity } from "./student.entity";
+import { LibrarianEntity } from "./librarian.entity";
+import { BookEntity } from "./book.entity";
+
+@Entity({ name: 'loans' })
+export class LoanEntity extends BaseEntity {
+    @OneToOne(() => StudentEntity, (entity) => entity.loan)
+    student!: StudentEntity;
+
+    @OneToOne(() => LibrarianEntity, (entity) => entity.loan)
+    librarian!: LibrarianEntity;
+
+    @OneToOne(() =>  BookEntity, (entity) => entity.order)
+    book!: BookEntity;
+}
