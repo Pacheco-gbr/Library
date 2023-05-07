78702e07a448ba297f54b9d8833e43781e19cb1a
Gabriel Pacheco Passos da Silva <Pacheco_gbr@hotmail.com>
2023-05-07 19:44:03 -0300

    Changing entities

--- src/app/shared/infra/db/entities/loan.entity.ts
+++ src/app/shared/infra/db/entities/loan.entity.ts
@@ -1,17 +1,20 @@
-import { Entity, OneToOne } from "typeorm";
+import { Entity, JoinColumn, OneToOne } from "typeorm";
 import BaseEntity from "./base-entity.entity";
 import { StudentEntity } from "./student.entity";
 import { LibrarianEntity } from "./librarian.entity";
 import { BookEntity } from "./book.entity";
 
-@Entity({ name: 'loans' })
+@Entity({ name: "loans" })
 export class LoanEntity extends BaseEntity {
-    @OneToOne(() => StudentEntity)
+    @OneToOne(() => StudentEntity, (entity) => entity.loan)
+    @JoinColumn({ name: "student_id", referencedColumnName: "id" })
     student!: StudentEntity;
 
     @OneToOne(() => LibrarianEntity, (entity) => entity.loan)
+    @JoinColumn({ name: "librarian_id", referencedColumnName: "id" })
     librarian!: LibrarianEntity;
 
-    @OneToOne(() =>  BookEntity, (entity) => entity.loan)
+    @OneToOne(() => BookEntity, (entity) => entity.loan)
+    @JoinColumn({ name: "book_id", referencedColumnName: "id" })
     book!: BookEntity;
 }
