ee656137ba635a27213996c5e25b3634f092f895
Gabriel Pacheco Passos da Silva <Pacheco_gbr@hotmail.com>
2023-05-07 16:18:13 -0300

    Creating the librarians table

--- src/app/shared/infra/db/migrations/1683413661379-CreateTableUsers.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class CreateTableUsers1683413661379 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {}
-
-    public async down(queryRunner: QueryRunner): Promise<void> {}
-}
