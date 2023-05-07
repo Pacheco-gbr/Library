de2a18c74441cf711b476fa211edaa3fd9604a10
Gabriel Pacheco Passos da Silva <Pacheco_gbr@hotmail.com>
2023-05-07 17:05:12 -0300

    Adding import in the index.ts inside migrations

--- src/app/shared/infra/db/migrations/index.ts
+++ src/app/shared/infra/db/migrations/index.ts
@@ -1,3 +1,7 @@
 import { CreateTableLibrarians1683414478956 } from "./1683414478956-CreateTableLibrarians";
+import { CreateTableStudents1683489237495 } from "./1683489237495-CreateTableStudents";
 
-export default [CreateTableLibrarians1683414478956];
+export default [
+    CreateTableLibrarians1683414478956,
+    CreateTableStudents1683489237495,
+];
